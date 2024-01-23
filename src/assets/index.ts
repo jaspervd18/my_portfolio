b4a225cece1c8a8f8183f45f76c52a43d243bb6e
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2024-01-23 23:33:22 +0100

    hero and navbar

---
+++ src/assets/index.ts
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import website from "./website.png";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import sql from "./tech/sql.png";
+import csharp from "./tech/csharp.png";
+import java from "./tech/java.png";
+
+import infosupport from "./company/infosupport.png";
+import hogent from "./company/hogent.png";
+
+import b2b from "./b2b.png";
+import stardew from "./stardew.png";
+import klauwaerts from "./klauwaerts.png";
+
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import github from "./github.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  website,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  sql,
+  csharp,
+  java,
+  typescript,
+  infosupport,
+  hogent,
+  b2b,
+  stardew,
+  klauwaerts,
+  facebook,
+  instagram,
+  linkedin,
+};
